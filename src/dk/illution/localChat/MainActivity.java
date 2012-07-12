4a344e64dd93f5f7c25b836201358efa5e24164b
fredefl <frederiklassen@gmail.com>
2012-07-12 15:52:39 +0200

    Made everything work nicely.
    Code cleanup, fixed errors, enhanced layout, tested and compiled APK.

--- src/dk/illution/localChat/MainActivity.java
+++ src/dk/illution/localChat/MainActivity.java
@@ -6,22 +6,17 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Enumeration;
 
-import android.R.string;
 import android.app.Activity;
-import android.content.Context;
-import android.net.DhcpInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Handler;
 
 
 public class MainActivity extends Activity implements OnClickListener {
@@ -29,126 +24,145 @@ public class MainActivity extends Activity implements OnClickListener {
 	Button sendButton;
 	public TextView log;
 	TextView messageBox;
-	DatagramSocket socket;
+	DatagramSocket senderSocket;
 	Handler handler;
-	
+
 	// On Create
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        // Set on click listener
-        sendButton = (Button) findViewById(R.id.sendButton);
-        log = (TextView) findViewById(R.id.log);
-        messageBox = (TextView) findViewById(R.id.messageBox);
-        sendButton.setOnClickListener(this);
-        initialize();
-        receive();
-        log.setFocusable(false);
-        handler = new Handler(getApplicationContext().getMainLooper());
-    } 
-    
-    // On Click
-    public void onClick(View v) {
-    	switch (v.getId()) {
-    	    case R.id.sendButton: 
-    	    	sendMessage();
-    	    break;
-    	}
-    }
-    
-    
-    public void initialize () {
-    	try {
-			socket = new DatagramSocket(6464);
-			socket.setBroadcast(true);
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// Set on click listener
+		sendButton = (Button) findViewById(R.id.sendButton);
+		log = (TextView) findViewById(R.id.log);
+		messageBox = (TextView) findViewById(R.id.messageBox);
+		messageBox.setOnEditorActionListener(textViewActionListener);
+		sendButton.setOnClickListener(this);
+		// Set up sender socket
+		initialize();
+		// Set up receiver socket
+		receive();
+		// Get the GUI handler
+		handler = new Handler(getApplicationContext().getMainLooper());
+	}
+
+
+	TextView.OnEditorActionListener textViewActionListener = new TextView.OnEditorActionListener(){
+
+		public boolean onEditorAction(TextView exampleView, int actionId, KeyEvent event) {
+			sendMessage();
+			return true;
 		}
-    }
-    
-    public void sendMessage () {
-    	String data = (String) messageBox.getText().toString();
+	};
+
+	// On Click
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.sendButton:
+				// Send the message
+				sendMessage();
+			break;
+		}
+	}
+
+
+	public void initialize () {
 		try {
-	    	DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(),
-	    	InetAddress.getByName("255.255.255.255"), 2700);
-	    	socket.send(packet);
-	    	/*
-	    	byte[] buffer = new byte[512];
-	    	DatagramPacket p = new DatagramPacket(buffer, buffer.length);
-	    	socket.receive(p);
-	    	log.append(new String(p.getData(), 0, p.getLength()));
-	    	*/
+			// Set up socket and send from port 6464
+			senderSocket = new DatagramSocket(6464);
+			// Make sure broadcast is enabled
+			senderSocket.setBroadcast(true);
 		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			log.append("Socket error\r\n");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			log.append("IO Error\r\n");
+			Log.e("LocalChat", "Error setting up sender socket", e);
 		}
-		
-    }
-    
-    public String getLocalIpAddress() {
-        try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-                NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-                    InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress()) {
-                        return inetAddress.getHostAddress().toString();
-                    }
-                }
-            }
-        } catch (SocketException ex) {
-        	// Darn it
-        }
-        return null;
-    }
-    
-    String text = "";
-    
-    public void logMessage (final String text) {
-    	handler.post(new Runnable() {
-    	    public void run() {
-    	    	log.append(text); 
-    	    }
-    	});
-    	
-    }
-    
-    public void receive() {
-	    new Thread(new Runnable() {
+	}
+
+	public void sendMessage () {
+		// Get text from TextView/messageBox
+		String text = (String) messageBox.getText().toString();
+		try {
+			// Create the UDP packet
+			DatagramPacket packet = new DatagramPacket(
+				text.getBytes(),
+				text.length(),
+				InetAddress.getByName("255.255.255.255"),
+				2700
+			);
+			// Send the packet!
+			senderSocket.send(packet);
+			// Free some memory
+			packet = null;
+			text = null;
+			messageBox.setText("");
+		} catch (SocketException e) {
+			Log.e("LocalChat", "Socket error", e);
+		} catch (IOException e) {
+			Log.e("LocalChat", "IO Error", e);
+		}
+
+	}
+
+	public String getLocalIpAddress() {
+		try {
+			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+				NetworkInterface intf = en.nextElement();
+				for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+					InetAddress inetAddress = enumIpAddr.nextElement();
+					if (!inetAddress.isLoopbackAddress()) {
+						en = null;
+						intf = null;
+						enumIpAddr = null;
+						return inetAddress.getHostAddress().toString();
+					}
+				}
+			}
+		} catch (SocketException e) {
+			// Darn it
+			Log.e("LocalChat", "Error getting local IP address", e);
+		}
+		return null;
+	}
+
+	public void logMessage (final String text) {
+		handler.post(new Runnable() {
 			public void run() {
-	            log = (TextView) findViewById(R.id.log);
-	            try {
-	                DatagramSocket clientsocket=new DatagramSocket(2700);
-	                byte[] receivedata = new byte[1024];
-	                while(true)
-	                {
-	                  DatagramPacket recv_packet = new DatagramPacket(receivedata, receivedata.length);
-	                  try {
-	                	  clientsocket.receive(recv_packet);
-	                	  String who = "";
-	                	  if(recv_packet.getAddress().toString().replace("/", "").equals(getLocalIpAddress())) {
-	                		  who = "You";
-	                	  } else {
-	                		  who = "Someone";
-	                	  };
-	                	  logMessage(who + ": " + new String(receivedata, 0, recv_packet.getLength()) + "\r\n");
-	                  } catch (Exception e) {
-	                	  Log.e("UDP", "S: Error", e);
-	                  }
-	                }
-	              } catch (Exception e) {
-	                Log.e("UDP", "S: Error", e);
-	                log.append("Error");
-	              }
-	    	} 
-    	}
-	).start();
-}
-}
+				log.append(text);
+			}
+		});
+
+	}
+
+	public void receive() {
+		new Thread(new Runnable() {
+			public void run() {
+				log = (TextView) findViewById(R.id.log);
+				try {
+					DatagramSocket receiverSocket = new DatagramSocket(2700);
+					byte[] receivedData = new byte[1024];
+					while (true) {
+						DatagramPacket packet = new DatagramPacket(receivedData, receivedData.length);
+						try {
+							receiverSocket.receive(packet);
+							String senderName = "";
+							if(packet.getAddress().toString().replace("/", "").equals(getLocalIpAddress())) {
+								senderName = "You";
+							} else {
+								senderName = "Someone";
+							}
+							logMessage(senderName + ": " + new String(receivedData, 0, packet.getLength()) + "\r\n");
+							senderName = null;
+						} catch (Exception e) {
+							Log.e("LocalChat", "Recieve error", e);
+						}
+						packet = null;
+					}
+				} catch (Exception e) {
+					Log.e("LocalChat", "Can't create receiver socket", e);
+					log.append("Error");
+				} finally {
+					log = null;
+				}
+			}
+		}).start();
+	}
+};
